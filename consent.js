ed546f2d19ade7b31c408d257a46b5c609bd0c1c
Maarten <m.j.a.duijndam@uu.nl>
2022-05-10 10:27:50 +0200

    Add elaborate consent text from het-bereden-paard

--- consent.js
+++ consent.js
@@ -8,13 +8,77 @@ let consent_given = false;
  * It is displayed as html, the current html should be replace with
  * your information letter.
  */
-const CONSENT_HTML = 
-    '<p>' +
-        'Insert your information letter here; for more information, see the '  +
-        '<a href="https://fetc-gw.wp.hum.uu.nl/en/" target="_blank"> '         +
-            'FEtC-H website'                                                   +
-        '</a>'                                                                 +
+const CONSENT_HTML =
+    "<p>" +
+    '<b>Welkom bij het online experiment Het Bereden Paard 22-028-03!</b>' +
+    "</p>" +
+    '<p>'			+
+    'Beste lezer,' +
+    '</p>'		+
+    '<p>'		+
+    'Wij, Betül Boz, Marit Schilling, Ronja van Zijverden en Tess Wensink, zijn studenten '	+
+    'aan de Universiteit Utrecht en nodigen je uit om deel te nemen aan ons onderzoek, dat '	+
+    'wordt uitgevoerd binnen de faculteit Geesteswetenschappen van de Universiteit Utrecht. '	+
+    'Dit wordt gedaan in het kader van de cursus Experimental Design and Data Analysis onder toezicht '	+
+    'van dr. Iris Mulders, cursuscoördinator.' +
+    '</p>'	+
+    '<p>'	+
+    '<b> 1.	Hoe wordt dit onderzoek uitgevoerd? </b>' +
+    '</p>'	+
+    '<p>'			+
+    'Dit is een anoniem wetenschappelijk onderzoek dat online wordt uitgevoerd. ' +
+    'Bij het onderzoek wordt je gevraagd om in stilte zinnen te lezen. Hierbij zal '  +
+    'je leestijd worden gemeten. Voorafgaand aan het experiment zal gevraagd worden '  +
+    'naar persoonlijke gegevens, verder gespecificeerd onder sectie 3 (Databeheer). ' +
+    'Na de test worden nog enkele vragen gesteld over het experiment zelf. Het onderzoek ' +
+    'duurt in totaal niet meer dan 20 minuten. Het is belangrijk dat je deelneemt aan het ' +
+    'onderzoek op een laptop of PC met toetsenbord, omdat het niet goed werkt op een tablet ' +
+    'of smartphone. Als je moedertaal niet Nederlands is, je een vorm van autisme, dyslexie ' +
+    'of een taalontwikkelingsstoornis (TOS) hebt, dan kun je helaas niet deelnemen aan dit '  +
+    'onderzoek. Het experiment wordt dan beëindigd en je gegevens worden niet opgeslagen.' +
+    '</p>'		+
+    '<p>'		+
+    '<b>2.	Vrijwillige deelname en anonimiteit</b>' +
+    '</p>'		+
+    '<p>'		+
+    'Deelname aan ons onderzoek is geheel vrijwillig. Alleen als je toestemming geeft, ' +
+    'zullen je gegevens worden gebruikt voor ons onderzoek. Gedurende het onderzoek mag ' +
+    'je op elk moment stoppen als je van gedachten verandert. Als je stopt, zal de data die ' +
+    'tot dan toe is ingevoerd niet opgeslagen worden. De data wordt anoniem verzameld. Je naam, ' +
+    'e-mailadres, IP-adres en tijd van afname worden niet geregistreerd.' +
+    '</p>'	+
+    '<p>'		+
+    '<b>3.	Databeheer</b>' +
+    '</p>'		+
+    '<p>'		+
+    'Voor dit experiment zullen we bepaalde gegevens van je gebruiken, namelijk je antwoorden '	+
+    'op de vragen die we voor en na het experiment stellen en je resultaten tijdens het experiment. '	+
+    'We slaan na het experiment de volgende gegevens op: je geboortemaand, geboortejaar, geslacht, '	+
+    'opleidingsniveau, met welke hand je schrijft, het apparaat en het besturingssysteem dat je '	+
+    'gebruikt voor het deelnemen aan het experiment, en je responstijden. Verder heb je aangegeven '	+
+    'dat je Nederlands als moedertaal hebt en dat je geen dyslexie, TOS of autisme hebt. Deze gegevens ' 	+
+    'zijn niet tot jou te herleiden. We zullen alleen meerkeuzevragen stellen. De gegevens zullen '	+
+    'minstens tien jaar opgeslagen worden op een beveiligde server van de Universiteit Utrecht, '	+
+    'waarbij ze eventueel beschikbaar zullen zijn voor gebruik door andere onderzoekers. Dit zal voor '	+
+    'niet-commercieel gebruik zijn.' +
+    '</p>'		+
+    '<p>'		+
+    '<b>4.	Goedkeuring van dit onderzoek</b>' +
+    '</p>'		+
+    '<p>'		+
+    'De Facultaire Ethische Toetsingscommissie - Geesteswetenschappen (FETC-GW) van de Universiteit '	+
+    'Utrecht heeft dit onderzoek goedgekeurd. Wanneer je een klacht hebt over de manier waarop dit '	+
+    'onderzoek wordt uitgevoerd, dan kun je contact opnemen met de secretaris van de '	+
+    'FETC-GW fetc-gw@uu.nl.' +
+    '</p>'		+
+    '<p>'		+
+    '<b>5.	Toestemming</b>' +
+    '</p>'		+
+    '<p>'		+
+    'Als je meer informatie wilt over dit onderzoek, kun je contact opnemen met dr. Iris Mulders '	+
+    '(I.C.M.C.Mulders@uu.nl) of Tess Wensink (t.s.b.wensink@uu.nl).' +
     '</p>';
+
 
 /*
  * Debrieving given when the participant doesn't consent.
@@ -28,7 +92,14 @@ const DEBRIEF_MESSAGE_NO_CONSENT =
     "</h2>";
 
 const CONSENT_STATEMENT = 
-    'Yes, I consent to the use of my answers for scientific research.';
+    `<b>
+        Ja, ik heb het bovenstaande gelezen en begrepen, en ik geef
+        uitdrukkelijke toestemming om mijn antwoorden (inclusief antwoorden op
+        persoonlijke vragen) te gebruiken voor wetenschappelijk onderzoek, zoals
+        hierboven beschreven. Als dit vakje niet aan wordt gevinkt, kan je niet
+        deelnemen aan het onderzoek.
+    </b>`;
+
 
 const CONSENT_REFERENCE_NAME = 'consent';
 const IF_REQUIRED_FEEDBACK_MESSAGE = 
